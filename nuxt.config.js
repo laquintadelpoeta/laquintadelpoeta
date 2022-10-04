5d88f911b8fcb7f436a35203aec0e5f1a9e19f0c
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2022-10-04 14:45:23 +0300

    Version updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,11 @@
-import { defineNuxtConfig } from 'nuxt';
-
 export default defineNuxtConfig({
-    css: [
-        'primevue/resources/themes/saga-blue/theme.css',
-        'primevue/resources/primevue.css',
-        'primeicons/primeicons.css'
-    ],
-    build: {
-        transpile: ['primevue']
-    }
+  css: [
+    'primevue/resources/themes/saga-blue/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css',
+    'primeflex/primeflex.css'
+  ],
+  build: {
+    transpile: ['primevue']
+  }
 })
