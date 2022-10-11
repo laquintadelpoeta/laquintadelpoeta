e67a10f1ad0790c480891da2a962832153ab242c
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-10-11 11:35:41 +0300

    indent fix

--- app.vue
+++ app.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="container">
-    <div class="flex flex-column align-items-center mt-1">
-      <img alt="Vue logo" src="./assets/primevue-logo.png" />
-      <Toast />
+	<div class="container">
+		<div class="flex flex-column align-items-center mt-1">
+			<img alt="Vue logo" src="./assets/primevue-logo.png" />
+			<Toast />
 
-      <div class="mt-4">
-        <form @submit.prevent="greet">
-          <InputText type="text" v-model="text" />
-          <Button type="submit" label="Submit" />
-        </form>
-      </div>
-    </div>
-  </div>
+			<div class="mt-4">
+				<form @submit.prevent="greet">
+					<InputText type="text" v-model="text" />
+					<Button type="submit" label="Submit" />
+				</form>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -21,16 +21,16 @@ import { useToast } from "primevue/usetoast";
 const text = ref();
 const toast = useToast();
 const greet = () => {
-  toast.add({ severity: "info", summary: "Hello " + text.value });
+	toast.add({ severity: "info", summary: "Hello " + text.value });
 };
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -5,10 +5,10 @@ import Toast from 'primevue/toast'
 import ToastService from 'primevue/toastservice'
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(PrimeVue, { ripple: true })
-  nuxtApp.vueApp.use(ToastService)
-  nuxtApp.vueApp.component('Button', Button)
-  nuxtApp.vueApp.component('InputText', InputText)
-  nuxtApp.vueApp.component('Toast', Toast)
-  //other components that you need
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.use(ToastService)
+    nuxtApp.vueApp.component('Button', Button)
+    nuxtApp.vueApp.component('InputText', InputText)
+    nuxtApp.vueApp.component('Toast', Toast)
+    //other components that you need
 })
